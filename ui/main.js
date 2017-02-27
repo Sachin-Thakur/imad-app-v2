1768eb79b6c061aac383a9dec078c2611c69520d
Sachin-Thakur <Sachin-Thakur@users.noreply.github.com>
2017-02-27 23:12:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,13 +9,13 @@ element.innerHTML= 'New Value';
 var img= document.getElementById('madi');
 
 var marginLeft = 0;
-function moveRight(){
+function moveRight () {
     marginLeft - marginLeft +10;
       img.style.marginLeft - marginLeft + 'px';
 }
  
 img.onclick = function() {
   
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight, 100);
     
 }; 
