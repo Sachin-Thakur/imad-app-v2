a4d3358bb4aecf53de7eda6c0adf2d0fd574f386
Sachin-Thakur <Sachin-Thakur@users.noreply.github.com>
2017-02-28 23:13:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -174,8 +174,9 @@ app.get('/ui/madi.png', function (req, res) {
 
 var names =[];
 
-app.get('/submit-name/:name', function(req,res){
-    var name = req.params.name;
+app.get('/submit-name/:name', function(req,res){ 
+    
+    var name = req.query.name;
     //JSON javascript object notation
     
     
