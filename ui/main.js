a922b04200bfa440543ae3c3cd55b1fd6c8f9a22
Sachin-Thakur <Sachin-Thakur@users.noreply.github.com>
2017-02-28 21:17:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,36 @@
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 
 
 button.onclick = function () {
     //make a request to the counter 
     
+    var request = new XMLHttpRequest();
+    
     //capture the responce store in a variable
     
-    //render the variable  in the correct span 
+    request.onreadystatechange= function() {
+        
+        if (request.readyState === XMLHttpRequest.DONE) {
+            
+            //take some action
+            if(request.status===200) {
+            var counter =     request.responceText;
+             var span = document.getElementById('count');
+            span.innerHTML= counter.toString();
+    
+            }
+            
+        }
+        
+        
+    };
+    
+    request.open('GET', 'http://sachin-thakur.imad.hasura-app.io/counter',true);
+    request.send(null);
     
     
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML= counter.toString();
     
     
     
